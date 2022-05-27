a6697444ba3c3b435a2c98d17dd8f94102c86f1d
alvaroragazzi <alvaroragazzi@gmail.com>
2022-05-27 00:16:24 -0300

    adicionado

---
+++ models/cliente.js
@@ -0,0 +1,35 @@
+const db = require("../database");
+
+module.exports = class Cliente {
+    static getAll(usuario_criou) {
+        const query = `SELECT * FROM analytcoz.clientes WHERE usuario_criou = $1`;
+
+        return db.query(query, [usuario_criou]);
+    }
+
+    static get(usuario_criou, id) {
+        const query = `SELECT * FROM analytcoz.produtos WHERE usuario_criou = $1 AND id = $2`;
+
+        return db.query(query, [usuario_criou, id]);
+    }
+
+    static insert(nome, valor, usuario_criou) {
+        const query = `
+        INSERT INTO analytcoz.produtos(
+            nome,
+            valor,
+            usuario_criou
+        ) VALUES(
+            $1,
+            $2,
+            $3
+        )
+        `
+
+        return db.query(query, [nome, valor, usuario_criou]);
+    }
+
+    static update() {
+
+    }
+}
