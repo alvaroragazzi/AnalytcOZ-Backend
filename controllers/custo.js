f52657dc5d850f3c8793b4f2080cbe76db6e7447
alvaroragazzi <alvaroragazzi@gmail.com>
2022-05-28 01:39:26 -0300

    insert no banco funcionando

---
+++ controllers/custo.js
@@ -0,0 +1,42 @@
+const CustoModel = require("../models/custo");
+
+exports.getAll = async function(req, res) {
+    try {
+        const result = await CustoModel.getAll(req.session.authenticated.id);
+
+        return res.send(result);
+    } catch {
+        return res.sendStatus(500);
+    }
+}
+
+exports.get = async function(req, res) {
+    try {
+        const result = await CustoModel.get(req.session.authenticated.id, req.params.id);
+
+        return res.send(result);
+    } catch {
+        return res.sendStatus(500);
+    }
+}
+
+exports.insert = async function(req, res) {
+    try {
+        const info = req.body;
+        const data_lancamento = info.data_lancamento;
+        const descricao = info.descricao;
+        const valor = info.valor;
+        const fornecedor = info.fornecedor;
+        const usuario_criou = req.session.authenticated.id;
+
+        await CustoModel.insert(data_lancamento, descricao, valor, fornecedor, usuario_criou);
+
+        return res.sendStatus(201);
+    } catch {
+        return res.sendStatus(500);
+    }
+}
+
+exports.update = async function(req, res) {
+    
+}
